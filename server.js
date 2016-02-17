ca4b8fb0a8d9a2b03ccf94697553f65a581ced35
Nuno Monteiro <nfmmonteiro@gmail.com>
2016-02-17 00:55:50 +0000

    Fixes could9 configuration of IP and PORT

--- server.js
+++ server.js
@@ -13,9 +13,11 @@ app.get('*', function(req, res) {
     res.render('index');
 });
 
-var port = process.env.PORT || 8080;
-var ip = process.env.IP || 'localhost';
+var port =  process.env.PORT || 9090;
+var ip = process.env.IP || '127.0.0.1';
 
-app.listen(port, ip, function() {
-    console.log(ip, 'listening on port', port);
+app.listen(port, ip, function() { 
+    console.log('[backlog]');
+}, function() {
+    console.log('[callback] ', ip, 'listening on', port);
 });
