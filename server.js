524d73c4b95efd104a1d42b42fdf4e1bd245524e
Nuno Monteiro <nfmmonteiro@gmail.com>
2016-02-18 00:56:06 +0000

    Exposes /jobs endpoint that returns all the jobs in the database.

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
 var express = require('express');
-
+var mongoose = require('mongoose');
+var JobModel = require('./models/JobModel');
 var app = express();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'))
 
+mongoose.connect('mongodb://localhost/jobsFinder');
+mongoose.connection.once('open', function() {
+	console.log('Connected to mongodb successfully');
+});
+
+app.get('/jobs', function(req, res) {
+	JobModel.find({}, function(err, data) {
+		if (err) {
+			res.status(500).end();
+		} else {
+			res.send(data);
+		}
+	});
+});
+
 /**
  * serves all requests with index view (except static files)
  **/
-app.get('*', function(req, res) {
+app.get('*', function(req, res, next) {
     res.render('index');
 });
+
 
 var port = process.env.PORT || 8080;
 var ip = process.env.IP || 'foo';
